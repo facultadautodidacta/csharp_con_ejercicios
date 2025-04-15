0b5a25235282806948e2481ea56514dfdd26eb94
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-15 17:10:18 -0600

    Create ejercicio9.cs

---
+++ ejercicio9.cs
@@ -0,0 +1,14 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        * 9 - Escribe un programa
+        * que convierta una cadena a
+        * mayúsculas y muestra el resultado.
+        */
+        string cadena = "facultad";
+        string mayusculas = cadena.ToUpper();
+        Console.WriteLine(mayusculas);
+    }
+}
