14c61cda4522f8d1228b38482f672aaaeadfc744
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-15 17:06:51 -0600

    Create ejercicio7.cs

---
+++ ejercicio7.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 7 - Escribe un programa que
+         * concatene dos cadenas y muestre
+         * el resultado.
+         */
+
+        string cadena1 = "Hola ";
+        string cadena2 = "mundo";
+        string totalCadena = cadena1 + cadena2;
+        Console.WriteLine(totalCadena);
+    }
+}
