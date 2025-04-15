fda543aa007f178189ea232f85ecc8c27b78fca1
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-14 19:47:15 -0600

    Create ejercicio5.cs

---
+++ ejercicio5.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 5 - Escribe un programa que divida
+         * dos números decimales literales
+         * y muestra el resultado.
+         */
+
+        decimal numero1 = 10.5m;
+        decimal numero2 = 2.5m;
+
+        decimal resultado = numero1 / numero2;
+        Console.WriteLine(resultado);
+    }
+}
