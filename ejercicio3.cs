ad601b310575f4dae43d7832c259804ce1270996
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-14 19:45:31 -0600

    Create ejercicio3.cs

---
+++ ejercicio3.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 3 - Escribe un programa
+         * que reste dos números decimales
+         * y muestre el resultado.
+         */
+
+        double numero1 = 10;
+        double numero2 = 5;
+        double resultado = numero1 - numero2;
+        Console.WriteLine(resultado);
+    }
+}
