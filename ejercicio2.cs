eefb5dcabd67a7370301c0e1fa1366ec1e1db7e0
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-14 19:44:42 -0600

    Create ejercicio2.cs

---
+++ ejercicio2.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+          /*
+           * 2 - Escribe un programa que
+           * sume dos números enteros
+           * y muestre el resultado.
+         */
+        int numero1 = 5;
+        int numero2 = 10;
+        int resultado = numero1 + numero2;
+        Console.WriteLine(resultado);
+    }
+}
