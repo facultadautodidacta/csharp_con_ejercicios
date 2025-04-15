05be40fcdfa78a936002ed060d4a7f8255dc0d80
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-14 19:46:30 -0600

    Create ejercicio4.cs

---
+++ ejercicio4.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 4 - Escribe un programa
+         * que multiplique dos números enteros
+         * y muestre el resultado.
+         * Concatena la palabra resultado.
+         */
+
+        int numero1 = 10;
+        int numero2 = 10;
+        int resultado = numero1 * numero2;
+        Console.WriteLine("Resultado= " + resultado);
+    }
+}
