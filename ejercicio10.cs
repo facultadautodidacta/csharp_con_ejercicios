612dc5c6e848eadbf6c503e2c123a18045e2da20
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-15 17:11:23 -0600

    Create ejercicio10.cs

---
+++ ejercicio10.cs
@@ -0,0 +1,14 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        * 10 - Escribe un programa que
+        * convierta una cadena a minúsculas
+        * y muestra el resultado.
+        */
+        string cadena1 = "FACULTAD";
+        string resultado = cadena1.ToLower();
+        Console.WriteLine(resultado);
+    }
+}
