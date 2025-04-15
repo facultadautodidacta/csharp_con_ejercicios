3a0db84e0961fdc3f635fe2b119bc7d23f755a35
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-15 17:05:58 -0600

    Create ejercicio6.cs

---
+++ ejercicio6.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 6 - Escribe un programa que
+         * calcule el módulo de dos números enteros
+         * y muestra el resultado.
+         */
+
+        int numero1 = 10;
+        int numero2 = 3;
+        int resultado = numero1 % numero2;
+        Console.WriteLine(resultado);
+    }
+}
