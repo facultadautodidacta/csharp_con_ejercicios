966106ed5d663a1b2922332a623b5a8819f00679
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-15 17:09:03 -0600

    Create ejercicio8.cs

---
+++ ejercicio8.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 8 - Escribe un programa que
+         * calcule la longitud de una cadena
+         * y muestre el resultado.
+         */
+
+        string cadena = "facultad autodidacta";
+        int longitud = cadena.Length;
+        Console.WriteLine(longitud);
+    }
+}
