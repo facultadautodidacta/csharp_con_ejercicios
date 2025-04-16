a1a8552b055bc513ba5f18c8e24b51e0d68cff26
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-16 15:32:18 -0600

    Create ejercicio11.cs

---
+++ ejercicio11.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 11 – Pide al usuario un
+         * string por teclado e
+         * imprime el resultado
+         */
+
+        Console.WriteLine("Escribe tu nombre");
+        string nombre = Console.ReadLine();
+        Console.WriteLine(nombre);
+    }
+}
