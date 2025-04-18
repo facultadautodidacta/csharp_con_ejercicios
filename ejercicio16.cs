2f433e442bf08d77c72cda9f3303597d54d0e404
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-18 11:21:11 -0600

    Create ejercicio16.cs

---
+++ ejercicio16.cs
@@ -0,0 +1,14 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        * 16 - Declara una variable
+        * con tipo de dato implícito
+        * y obten el tipo de dato
+        * de la misma.
+        */
+        var variable1 = 10;
+        Console.WriteLine(variable1.GetType());
+    }
+}
