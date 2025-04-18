1fd479b6a212badb9fa95fd659734641fa1e98f2
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-18 11:21:57 -0600

    Create ejercicio19.cs

---
+++ ejercicio19.cs
@@ -0,0 +1,14 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        * 19 - Declara una variable
+        * de tipo DateTime
+        * y asignale la fecha
+        * y hora actual
+        */
+        DateTime fechahora = DateTime.Now;
+        Console.WriteLine(fechahora);
+    }
+}
