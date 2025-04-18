80ab06008e4ef044ceb5cfebfb6b291c1e336d56
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-18 11:21:25 -0600

    Create ejercicio17.cs

---
+++ ejercicio17.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        * 17 - Declara una constante
+        * y asignale un valor.
+        *
+        * Las constantes son útiles cuando
+        * tienes valores que no deben cambiar,
+        * como constantes matemáticas,
+        * configuraciones fijas, etc.
+        */
+        const double pi = 3.1416;
+        Console.WriteLine(pi);
+    }
+}
