3144daf692cba80cb8619ec2fd9cb313abfd8fa7
Facultad Autodidacta <33157200+facultadautodidacta@users.noreply.github.com>
2025-04-25 13:43:59 -0600

    Create ejercicio_38.cs

---
+++ ejercicio_38.cs
@@ -0,0 +1,18 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 38 -  Escribe un programa
+         * que entre en un 
+         * bucle infinito y 
+         * salga con break.
+         */
+
+
+        while (true)
+        {
+            break;
+        }
+    }
+}
